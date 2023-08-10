62deb27127e196fefc37083609d715b3e040f582
Gimhan-minion <gimhan@entgra.io>
2023-08-10 15:20:29 +0530

    Initial commit

---
+++ src/main/java/io/g2labs/goldeni/GoldenIApplication.java
@@ -0,0 +1,13 @@
+package io.g2labs.goldeni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldenIApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/g2labs/goldeni/GoldenIApplicationTests.java
@@ -0,0 +1,13 @@
+package io.g2labs.goldeni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
